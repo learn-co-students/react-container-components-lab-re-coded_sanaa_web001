56e31524b54667cb9b44ab2a0fc9a527e71f2d0b
Shihab AL-Mekhlafi <shihabalmekhlafy@gmail.com>
2020-10-06 00:18:39 +0000

    Done.

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,9 +1,38 @@
 import React, { Component } from 'react';
 import 'isomorphic-fetch';
-import MovieReviews from './MovieReviews'
+import MovieReviews from './MovieReviews';
 
 const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends Component {
+
+  state = {
+    searchTerm: '',
+    reviews: []
+  };
+  handleSearch = event =>
+    this.setState({ searchTerm: event.target.value });
+
+  Submit = event => {
+    event.preventDefault();
+
+    fetch(URL.concat(this.state.searchTerm))
+      .then(res => res.json())
+      .then(json => this.setState({ reviews: json.results }));
+  };
+
+  render() {
+    return (
+      <div className="searchable-movie-reviews">
+        <form onSubmit={this.Submit}>
+          <input type="text" onChange={this.handleSearch} />
+          <button type="submit">Submit</button>
+        </form>
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    );
+  }
+}
