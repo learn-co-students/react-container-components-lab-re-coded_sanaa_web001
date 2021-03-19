af388a06422026e1f81c433faf1cf846480a52db
ismailkhalil20 <ismail.th.khalil@gmail.com>
2021-03-19 16:46:50 +0300

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,12 @@
 // Code MovieReviews Here
+import React from 'react'
+
+export default function MovieReviews(props) {
+    return (
+        <div className="review-list">
+            {props.reviews.map(review => { return <div className="review">
+                                                            <h1>{review.display_title}</h1>
+                                                        </div>})}
+        </div>
+    )
+}
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -7,3 +7,34 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+
+export default class SearchableMovieReviewsContainer extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            searchTerm: '',
+            reviews: []
+        }
+    }
+
+    handlechange = e => {
+        this.setState({searchTerm: e.target.value})
+    }
+
+    componentDidUpdate() {
+        fetch(URL + `&query=${this.state.searchTerm}`)
+        .then(response => response.json())
+        .then(response => this.setState({reviews: response.results}))
+    }
+
+    render() {
+        return (
+            <div className="searchable-movie-reviews">
+                <form onSubmit={this.handlechange} >
+                    <input type='text' />
+                </form>
+                { this.state.reviews.length ? <MovieReviews reviews={this.state.reviews} /> : null}
+            </div>
+        )
+    }
+}
